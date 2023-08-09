8ba64351b915fd0c6c9c2dfafd1d18c6f7fae8ff
Abhishek Naik <abhisheknaikworkspace@gmail.com>
2023-08-09 20:43:39 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express  from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+var pass= "";
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({extended : true}))
+
+function secret(req,res,next){
+    console.log(req.body);
+    pass = req.body["password"];
+    if (pass == "ILoveProgramming"){
+        userIsAuthorised = true;
+    }
+    next();
+}
+
+app.use(secret)
+
+app.get('/' , (req , res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/check" ,(req, res) => {
+    if (userIsAuthorised){
+        res.sendFile(__dirname + "/public/secret.html");
+        userIsAuthorised = false;
+    }
+    else{
+        res.sendFile(__dirname + "/public/index.html");
+    }
+})
+
+app.listen(port , () => {
+    console.log(`Listening on port ${port}`)
+})
